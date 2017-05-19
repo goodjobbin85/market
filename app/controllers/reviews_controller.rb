093a1781d4ac39022b220577cc2bba421c87386c
Thomas Dubiel <timidturtle85@gmail.com>
2017-05-18 20:10:49 -0500

    review-ui

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,33 @@
 class ReviewsController < ApplicationController
+
+before_action :set_review
+
 	def index 
-		@item = Item.find(params[:item_id])
 		@reviews = @item.reviews
 	end
+
+	def new 
+		@review = @item.reviews.new
+	end 
+
+	def create 
+		@review = @item.reviews.new(review_params)
+		if @review.save
+			redirect_to item_reviews_path(@item),
+					notice: "Thanks for your review!"
+		else 
+			render :new
+		end
+	end
+
+	private 
+
+	def set_review
+		@item = Item.find(params[:item_id])
+	end
+
+	def review_params
+		params.require(:review).permit(:name, :comment, :stars)
+	end
+
 end
---
+++ spec/features/create_review_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "creating a new review" do 
+
+	it "saves the review" do 
+		item = Item.create(item_attributes)
+		visit item_url(item)
+		click_link "Write Review"
+		expect(current_path).to eq(new_item_review_path(item))
+
+		fill_in "Name", with: "Kasia"
+		select 3, :from => "review_stars"
+		fill_in "Comment", with: "Holy moly macaroni"
+
+		click_button "Post Review"
+
+		expect(page).to have_text("Thanks for your review!")
+	end 
+
+	it "does not save invalid review" do 
+		item = Item.create(item_attributes)
+		visit new_item_review_url(item)
+		expect {
+			click_button 'Post Review'
+		}.not_to change(Review, :count)
+	end
+end 
